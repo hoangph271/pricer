ea9e8ab64cb40a431b1bf751f2f9a6a192066da0
Phan Huy Hoang <hoangph271@gmail.com>
2021-07-08 15:23:13 +0700

    [Add] CORS

---
+++ pages/api/_api_helpers.ts
@@ -0,0 +1,16 @@
+import Cors from 'cors'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export const cors = Cors()
+
+export function runMiddleware (req: NextApiRequest, res: NextApiResponse, fn: Function) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
--- pages/api/coins/index.ts
+++ pages/api/coins/index.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { google } from 'googleapis'
 import fetch from 'node-fetch'
+import { cors, runMiddleware } from '../_api_helpers'
 
 const credentials = { installed: { client_id: '551315315389-6keh2vo91kh06ce6d1n21h8oha50l09q.apps.googleusercontent.com', project_id: 'crusty-194406', auth_uri: 'https://accounts.google.com/o/oauth2/auth', token_uri: 'https://oauth2.googleapis.com/token', auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs', client_secret: '7a2vnKGfpeytVNJrWFiuQs1K', redirect_uris: ['urn:ietf:wg:oauth:2.0:oob', 'http://localhost'] } }
 const token = { access_token: 'ya29.a0ARrdaM9-58kuGHUZgPp58a1J6sc5pyuwt8JHtTlqX8LFVOeHnCLc8yO82m6QYrwyqfjcmYk4us3LeZnCfYiGvPLSrirOsVJ-dC4J6qjdaCtRPDiCl99wpyzfkNTrJkUCjk43Ql8dESE3wS1IYLcIBi_X6GAY', refresh_token: '1//0ePaIsKB4KG0dCgYIARAAGA4SNwF-L9Irj0fj-tyqywc_xyHy8FDzVdE2VW7HAWFS-7UJUVud-QD9hRTaR-XQAbY82iOCsj6Z6M0', scope: 'https://www.googleapis.com/auth/spreadsheets.readonly', token_type: 'Bearer', expiry_date: 1625222830174 }
@@ -36,7 +37,9 @@ export type CoinStats = {
   balanceChanges: number,
   paidEntries: PaidEntry[]
 }
-export default async function handler (_: NextApiRequest, res: NextApiResponse) {
+export default async function handler (req: NextApiRequest, res: NextApiResponse) {
+  await runMiddleware(req, res, cors)
+
   const { client_id, client_secret, redirect_uris } = credentials.installed
   const auth = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0])
   auth.setCredentials(token)
