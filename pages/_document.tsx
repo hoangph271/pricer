ffcb54fad5bec70617439c3b6c264eef8babec0f
Alpha Snêu <39024711+hoangph271@users.noreply.github.com>
2021-10-02 15:16:43 +0700

    [Fix] Add icon

---
+++ pages/_document.tsx
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class PricerDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="shortcut icon" href="https://www.runicgames.com/images/hob-marquee-char.png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default PricerDocument
