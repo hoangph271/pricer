0115213ecaf670f82ab3d81fa64013433fae086b
hoang.phanhuy <hoang.phanhuy@vietis.com.vn>
2021-10-06 09:27:58 +0700

    [Fix] getServerSideProps with next-auth session

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react'
-import type { CoinStats } from './api/coins'
-import { signIn, useSession } from 'next-auth/react'
-import type { FC, PaidEntry } from '../global'
-import { formatUsd, formatVnd, formatMoney, formatDate } from '../lib/formatters'
 import Head from 'next/head'
+import { signIn, useSession } from 'next-auth/react'
+import { formatUsd, formatVnd, formatMoney, formatDate } from '../lib/formatters'
+
+import type { CoinStats } from './api/coins'
+import type { FC, PaidEntry } from '../global'
+import { GetServerSideProps } from 'next'
 
 const MoneyBadge: FC<{ usdAmount: number, usdPrice: number, title?: string }> = (props) => {
   const { usdAmount, usdPrice, title } = props
@@ -165,9 +167,13 @@ export default Home
 
 const { API_ROOT = 'http://0.0.0.0:3000/api' } = process.env
 
-export async function getServerSideProps () {
-  // TODO: fetch always 401 because session is not included
-  const res = await fetch(`${API_ROOT}/coins/`)
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const res = await fetch(`${API_ROOT}/coins/`, {
+    headers: {
+      cookie: ctx.req?.headers.cookie ?? ''
+    }
+  })
+
   console.info('/coins/', res.statusText)
 
   return {
