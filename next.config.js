28f940517bd9d6a937634a93e9b69848db16249d
Phan Huy Hoang <hoangph271@gmail.com>
2021-10-28 10:16:07 +0000

    [Fix] Remove CSS urlImports

--- next.config.js
+++ next.config.js
@@ -11,9 +11,6 @@ module.exports = withPwa({
   experimental: {
     concurrentFeatures: true,
     serverComponents: true,
-    urlImports: [
-      'https://fonts.googleapis.com/css?family=Press+Start+2P',
-      'https://unpkg.com/nes.css@2.3.0/css/nes.min.css'
-    ]
+    urlImports: []
   }
 })
