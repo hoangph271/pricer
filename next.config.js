3d8484ee7420b069a29a4c32a847139659b8f67b
Phan Huy Hoang <hoangph271@gmail.com>
2021-10-28 10:13:11 +0000

    [NextJS] 12

--- next.config.js
+++ next.config.js
@@ -2,9 +2,18 @@ const withPwa = require('next-pwa')
 
 module.exports = withPwa({
   reactStrictMode: true,
+  swcMinify: true,
   pwa: {
     dest: 'public',
     register: true,
     skipWaiting: true,
+  },
+  experimental: {
+    concurrentFeatures: true,
+    serverComponents: true,
+    urlImports: [
+      'https://fonts.googleapis.com/css?family=Press+Start+2P',
+      'https://unpkg.com/nes.css@2.3.0/css/nes.min.css'
+    ]
   }
 })
