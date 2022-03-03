cbeaad0753e070d922c6970452e95b9a77882ac8
Phan Huy Hoang <hoangph271@gmail.com>
2022-03-03 11:23:03 +0700

    [DOT]

--- pages/api/coins/_paid_entries/_paid_entries.dot.data.ts
+++ pages/api/coins/_paid_entries/_paid_entries.dot.data.ts
@@ -6,6 +6,7 @@ const spentDotEntries: CoinSpentEntry[] = [
   ['2021-11-13 17:24:03', 34.15457339, 0.75011746],
   ['2021-11-30 22:55:18', 19.9969, 0.53],
   ['2022-01-07 01:08:15', 26, 1],
+  ['2022-03-02 21:32:46', 37, 2],
 ]
 
 export const DOT: PaidEntry[] = spentDotEntries.map(toPaidEntry('DOT'))
