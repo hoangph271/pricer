b8f6951e270ecd9a0bc1e9caf29dc2f384d84f5e
Phan Huy Hoang <hoangph271@gmail.com>
2022-04-23 23:54:01 +0700

    [Pricing]

--- pages/api/coins/_paid_entries/_paid_entries.usdt.data.ts
+++ pages/api/coins/_paid_entries/_paid_entries.usdt.data.ts
@@ -3,7 +3,7 @@ import { CoinSpentEntry } from './_paid_entries.types'
 import { toPaidEntry } from './_paid_entries.utils'
 
 const spentUsdtEntries: CoinSpentEntry[] = [
-  ['2022-04-22 01:07:22', 17.8893, 1.1],
+  ['', 1.44507382, 1.44507382],
 ]
 
 export const USDT: PaidEntry[] = spentUsdtEntries.map(toPaidEntry('USDT'))
--- pages/api/coins/_paid_entries/_paid_entries.ust.data.ts
+++ pages/api/coins/_paid_entries/_paid_entries.ust.data.ts
@@ -3,7 +3,7 @@ import { CoinSpentEntry } from './_paid_entries.types'
 import { toPaidEntry } from './_paid_entries.utils'
 
 const spentUstEntries: CoinSpentEntry[] = [
-  ['2022-04-22 01:07:22', 17.8893, 1.1],
+  ['', 42.27435528, 42.27435528],
 ]
 
 export const UST: PaidEntry[] = spentUstEntries.map(toPaidEntry('UST'))
