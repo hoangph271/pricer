5f447ababe50735abde172037522b65b8fe0924c
Phan Huy Hoang <hoangph271@gmail.com>
2022-04-27 11:20:13 +0700

    [Remove] Stable coins

--- pages/api/coins/_paid_entries/_paid_entries.usdt.data.ts
+++ pages/api/coins/_paid_entries/_paid_entries.usdt.data.ts
@@ -3,7 +3,6 @@ import { CoinSpentEntry } from './_paid_entries.types'
 import { toPaidEntry } from './_paid_entries.utils'
 
 const spentUsdtEntries: CoinSpentEntry[] = [
-  ['', 1.44507382, 1.44507382],
 ]
 
 export const USDT: PaidEntry[] = spentUsdtEntries.map(toPaidEntry('USDT'))
--- pages/api/coins/_paid_entries/_paid_entries.ust.data.ts
+++ pages/api/coins/_paid_entries/_paid_entries.ust.data.ts
@@ -3,7 +3,6 @@ import { CoinSpentEntry } from './_paid_entries.types'
 import { toPaidEntry } from './_paid_entries.utils'
 
 const spentUstEntries: CoinSpentEntry[] = [
-  ['', 42.27435528, 42.27435528],
 ]
 
 export const UST: PaidEntry[] = spentUstEntries.map(toPaidEntry('UST'))
