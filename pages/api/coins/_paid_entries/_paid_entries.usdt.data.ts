ef7323eb73760b8c726d5b0711f135bfa6d2cc3a
Phan Huy Hoang <hoangph271@gmail.com>
2022-06-01 17:50:47 +0700

    [Add] USDT

--- pages/api/coins/_paid_entries/_paid_entries.usdt.data.ts
+++ pages/api/coins/_paid_entries/_paid_entries.usdt.data.ts
@@ -3,6 +3,7 @@ import { CoinSpentEntry } from './_paid_entries.types'
 import { toPaidEntry } from './_paid_entries.utils'
 
 const spentUsdtEntries: CoinSpentEntry[] = [
+  ['2022-06-01 00:00:00', 20, 20],
 ]
 
 export const USDT: PaidEntry[] = spentUsdtEntries.map(toPaidEntry('USDT'))
