edf3cabb313cef190abd183ca0197380bf84393f
Phan Huy Hoang <hoangph271@gmail.com>
2022-07-20 22:42:38 +0700

    [Fix] queryCoinNameOrDefault

--- pages/api/coins/_paid_entries/_paid_entries._trade.data.ts
+++ pages/api/coins/_paid_entries/_paid_entries._trade.data.ts
@@ -4,7 +4,7 @@ import { toPaidEntry } from './_paid_entries.utils'
 
 export const paidEntries: Record<string, PaidEntry[]> = {
   BMI: [
-    // ['2022-03-01 17:03:28', 25.592, 225],
+    ['2022-03-01 17:03:28', 25.592, 225],
   ].map(toPaidEntry('BMI') as any),
 }
 
