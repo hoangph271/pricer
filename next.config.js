c9976d221ff3a971ec4c78460434bdb19844962e
Phan Huy Hoang <hoangph271@gmail.com>
2022-07-20 22:11:32 +0700

    [Fix] Disable PWA

--- next.config.js
+++ next.config.js
@@ -4,12 +4,12 @@ const withPwa = require('next-pwa')
 module.exports = withPwa({
   reactStrictMode: true,
   swcMinify: true,
-  pwa: {
-    disable: process.env.NODE_ENV === 'development',
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-  },
+  // pwa: {
+  //   disable: process.env.NODE_ENV === 'development',
+  //   dest: 'public',
+  //   register: true,
+  //   skipWaiting: true,
+  // },
   experimental: {
     // ? TODO: When this bug is fixed, un-comment concurrentFeatures & serverComponents
     // * https://github.com/vercel/next.js/issues/30586
