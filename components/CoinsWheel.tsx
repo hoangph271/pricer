ccd954ada86c0d4908ab3cf8d9d2a93fa259a5df
Phan Huy Hoang <hoangph271@gmail.com>
2022-11-30 08:21:45 +0700

    [Add] Legends

--- components/CoinsWheel.tsx
+++ components/CoinsWheel.tsx
@@ -1,50 +1,61 @@
 import { useRouter } from 'next/router'
-import { useMemo } from 'react'
+import { useMemo, useState } from 'react'
 import { Doughnut } from 'react-chartjs-2'
-import { ArcElement, Chart, Tooltip, ChartData } from 'chart.js'
+import { ArcElement, Chart, Tooltip, ChartData, Legend } from 'chart.js'
 
 import { CoinStats } from '../pages/api/coins/_types'
 import { PaidEntry } from '../global'
 import { formatUsd } from '../lib/formatters'
 
-Chart.register(ArcElement, Tooltip)
+Chart.register(ArcElement, Tooltip, Legend)
+
+const randomChannelValue = () => Math.floor(Math.random() * (235 - 52 + 1) + 52)
+const randomRgb = () => `rgb(${randomChannelValue()}, ${randomChannelValue()}, ${randomChannelValue()})`
 
 export const CoinsWheel = ({ coinStats }: { coinStats: CoinStats }) => {
   const router = useRouter()
+  const [showAll] = useState(false)
 
   const chartData: ChartData<'doughnut', number[], unknown> = useMemo(() => {
-    const randomChannelValue = () => Math.floor(Math.random() * (235 - 52 + 1) + 52)
-    const randomRgb = () => `rgb(${randomChannelValue()}, ${randomChannelValue()}, ${randomChannelValue()})`
-    const sumEntries = (prev: number, val: PaidEntry) => val.amount * coinStats.prices[val.name] + prev
+    const sumEntries = (entries: PaidEntry[]) => {
+      return entries.reduce((prev, val) => val.amount * coinStats.prices[val.name] + prev, 0)
+    }
+    const sumSpentEntries = (entries: PaidEntry[]) => {
+      return entries.reduce((prev, val) => val.amountUsd + prev, 0)
+    }
 
-    const coinNames = Object.keys(coinStats.paids).sort((coinName1, coinName2) => {
-      const coinSum1 = coinStats.paids[coinName1].reduce(sumEntries, 0)
-      const coinSum2 = coinStats.paids[coinName2].reduce(sumEntries, 0)
+    const coinNames = Object.keys(coinStats.paids)
+      .filter((coinName) => {
+        const coinSum = sumSpentEntries(coinStats.paids[coinName])
 
-      return coinSum2 - coinSum1
-    })
+        return showAll || (coinSum > 80)
+      })
+      .sort((coinName1, coinName2) => {
+        const coinSum1 = sumEntries(coinStats.paids[coinName1])
+        const coinSum2 = sumEntries(coinStats.paids[coinName2])
+
+        return coinSum2 - coinSum1
+      })
     const paidEntries = coinNames.map(coinName => coinStats.paids[coinName])
 
     return {
       labels: coinNames,
       datasets: [{
-        data: paidEntries
-          .map((entries) => {
-            return entries.reduce(sumEntries, 0)
-          }),
-        backgroundColor: paidEntries
-          .map(() => randomRgb()),
+        data: paidEntries.map(sumEntries),
+        backgroundColor: paidEntries.map(randomRgb),
       }]
     }
-  }, [coinStats])
+  }, [coinStats, showAll])
 
   return (
     <div
-      style={{ height: '200px', width: '200px' }}
+      style={{ height: '200px', width: '600px', overflowX: 'auto', maxWidth: '100%' }}
     >
       <Doughnut
+        style={{ margin: 'auto', height: 400 }}
         data={chartData}
         options={{
+          aspectRatio: 3,
           onClick (_, activeElements) {
             const [{ index }] = activeElements
             const coinName = chartData.labels![index]
@@ -52,6 +63,9 @@ export const CoinsWheel = ({ coinStats }: { coinStats: CoinStats }) => {
             router.push(`/?coinName=${coinName}`)
           },
           plugins: {
+            legend: {
+              position: 'right',
+            },
             tooltip: {
               callbacks: {
                 label (context) {
